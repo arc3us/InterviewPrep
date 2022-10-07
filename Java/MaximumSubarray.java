2465ad6974e173e96d0ad44071c0682c1ab63c5b
Arinjay <55256534+arc3us@users.noreply.github.com>
2022-10-07 23:57:40 +0530

    Create MaximumSubarray.java

---
+++ Java/MaximumSubarray.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar=nums[0], maxEndingHere=nums[0];
+        for (int i=1;i<nums.length;++i){
+            maxEndingHere= Math.max(maxEndingHere+nums[i],nums[i]);
+            maxSoFar=Math.max(maxSoFar, maxEndingHere);	
+        }
+        return maxSoFar;
+    }
+}
