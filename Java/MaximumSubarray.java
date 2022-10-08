e02feaa1fd97536721120ffff81d6b25660de348
Vikasdeep Jangra <50790730+vikasdeepjangra@users.noreply.github.com>
2022-10-08 16:23:10 +0530

    Solution Using Kadane Algo
    Kr Do Merge Pls!
    
    T-Shirt Chahiye.

--- Java/MaximumSubarray.java
+++ Java/MaximumSubarray.java
@@ -10,3 +10,16 @@ class Solution {
         return maxSoFar;
     }
 }
+
+// USING KADANE ALGORITHM
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            sum = Math.max(nums[i], sum + nums[i]);
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
+}
